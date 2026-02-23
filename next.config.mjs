76bdc4a68f91ac34c598071de70aa9c9393af84d
Human Lab Deploy <deploy@human.lab>
2026-02-23 19:59:17 -0300

    chore: return to standard vercel flow and cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
